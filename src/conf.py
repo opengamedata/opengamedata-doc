f8a86ed6efc959ce60cb63e22082379df18e17d5
Luke Swanson <lwswanson2@wisc.edu>
2024-11-12 22:59:05 -0600

    Attempt to add logo to the readthedocs.

--- src/conf.py
+++ src/conf.py
@@ -82,6 +82,7 @@ exclude_patterns = [
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_logo = 'assets/extern/images/logos/ogd/OGD-128.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
