1e088ac11ae5e0e2898ffbbae6c02d7e818283a8
Luke Swanson <lwswanson2@wisc.edu>
2024-11-13 10:26:17 -0600

    Ignore templates folder from toctree checks.

--- src/conf.py
+++ src/conf.py
@@ -74,6 +74,7 @@ suppress_warnings = ["myst.header"]
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
+    "util/templates/*",
 ]
 
 # -- Options for HTML output -------------------------------------------------
